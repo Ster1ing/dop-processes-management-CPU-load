bfc62cded5554f063c4c5556f6137b8509d8bc77
Oleksander Tarakanov <Ster1ing@users.noreply.github.com>
2023-09-27 15:08:41 +0300

    Update processes.sh

--- processes.sh
+++ processes.sh
@@ -8,6 +8,7 @@ fulload() {
   dd if=/dev/zero of=/dev/null |
   dd if=/dev/zero of=/dev/null |
   dd if=/dev/zero of=/dev/null |
+  dd if=/dev/zero of=/dev/null |
   dd if=/dev/zero of=/dev/null &
 };
 
