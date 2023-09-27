f89950417e6da1ce6b7abf7093bf9fc1c5b50a8d
Oleksander Tarakanov <Ster1ing@users.noreply.github.com>
2023-09-27 14:51:25 +0300

    Update processes.sh

--- processes.sh
+++ processes.sh
@@ -3,6 +3,8 @@
 set -B
 set -x
 
+lscpu
+
 fulload() {
   dd if=/dev/zero of=/dev/null |
   dd if=/dev/zero of=/dev/null |
@@ -10,9 +12,4 @@ fulload() {
   dd if=/dev/zero of=/dev/null &
 };
 
-ld=0
-
-while [ "${ld} -ne 1" ]; do
-    fulload;     
-    ld=`cat /proc/loadavg | echo "$(awk -F " " '{ print $1 }') > 50" | bc`    
-done
+fulload;
