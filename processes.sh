d920f5d40e29aaaa9a07cfa11acba6ee915972b8
Oleksander Tarakanov <Ster1ing@users.noreply.github.com>
2023-09-27 14:41:57 +0300

    Update processes.sh

--- processes.sh
+++ processes.sh
@@ -7,17 +7,12 @@ fulload() {
   dd if=/dev/zero of=/dev/null |
   dd if=/dev/zero of=/dev/null |
   dd if=/dev/zero of=/dev/null |
-  dd if=/dev/zero of=/dev/null |
-  dd if=/dev/zero of=/dev/null |
-  dd if=/dev/zero of=/dev/null |
   dd if=/dev/zero of=/dev/null &
 };
 
 ld=0
 
 while [ "${ld} -ne 1" ]; do
-    #fulload; 
-    dd if=/dev/zero of=/dev/null &
-    ld=`cat /proc/loadavg | echo "$(awk -F " " '{ print $1 }') > 50" | bc`
-    echo "ld - $ld"
+    fulload;     
+    ld=`cat /proc/loadavg | echo "$(awk -F " " '{ print $1 }') > 50" | bc`    
 done
