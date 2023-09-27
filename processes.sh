414ba87423c76451d411f112d8bd9ce30112b805
Oleksander Tarakanov <Ster1ing@users.noreply.github.com>
2023-09-27 15:04:00 +0300

    Update processes.sh

--- processes.sh
+++ processes.sh
@@ -5,6 +5,6 @@ set -x
 ld=0
 
 while [ "${ld} -ne 1" ]; do
-    dd if=/dev/zero of=/dev/null &
+    dd if=/dev/zero of=/dev/null | dd if=/dev/zero of=/dev/null &
     ld=`cat /proc/loadavg | echo "$(awk -F " " '{ print $1 }') > 50" | bc`    
 done
